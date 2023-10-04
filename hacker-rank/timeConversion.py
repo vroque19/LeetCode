28bbe4a857fb9b889c00ea382ca99c408c72273d
Vanessa Roque <viroque19@gmail.com>
2023-10-04 09:39:16 +0000

    add timeConversion.py

---
+++ hacker-rank/timeConversion.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    hours = s[:2]
+    if int(hours) < 12 and "PM" in s:
+        hours = int(hours) + 12
+    elif int(hours) == 12 and "AM" in s:
+        hours = "00"
+    return str(hours) + s[2:8]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
