70d44de42c8588dd8694aa27238e642a2869cd95
Vanessa Roque <viroque19@gmail.com>
2023-10-11 11:21:25 +0000

    add diagonalDifference.py

---
+++ hacker-rank/diagonalDifference.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    n = len(arr[1])
+    sum_1 = sum([arr[i][i] for i in range(n)])
+    sum_2 = sum([arr[i][n-i-1] for i in range(n)])
+    
+    return (abs(sum_1 - sum_2))
+
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
