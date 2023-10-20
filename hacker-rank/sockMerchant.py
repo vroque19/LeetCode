1aaa5411ae97a7fa7cfe48e53b1a9002644cf1a1
Vanessa Roque <viroque19@gmail.com>
2023-10-20 10:52:53 +0000

    sockMerchant.py

---
+++ hacker-rank/sockMerchant.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    ar.sort()
+    pairs = 0
+    colors = set(ar)
+    # print(ar, "and", colors, "\n")
+    for color in colors:
+        # print(ar.count(color))
+        count = ar.count(color)
+        pairs = pairs + count // 2
+
+    return pairs
+        
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
