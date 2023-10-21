1a73be54710b6a927f8f142468db96058d2613ea
Vanessa Roque <viroque19@gmail.com>
2023-10-21 10:29:53 +0000

    add twoSumII.py - first medium

---
+++ neetcode150/twoSumII.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers)-1
+        for i in range(len(numbers)):
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                return [left+1, right+1]
