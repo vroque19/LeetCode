651c5ba47ae09abdf4cd144944815463f4d6aca5
Vanessa Roque <viroque19@gmail.com>
2023-10-21 10:14:47 +0000

    update maxScore.py

--- leetcode/maxScore.py
+++ leetcode/maxScore.py
@@ -1,9 +1,15 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        scores = []
-        for i in range(1, len(s)):
-            left = s[:i]
-            right = s[i:]
-            scores.append(right.count('1') + left.count('0'))
-        return max(scores)
-        
+        right_score = s.count('1')
+        max_score = 0
+        left_score = 0
+        for i in range(len(s)-1):
+            if s[i] == '0':
+                # a '0' is moved to the left half
+                left_score += 1
+            else:
+                # a '1' is moved to the left half
+                right_score -= 1
+            max_score = max(max_score, left_score + right_score) # max of max_score and tot curr score
+        return max_score
+    # time complexity: O(n) 
