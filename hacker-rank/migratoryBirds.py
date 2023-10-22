79e6d00fa7ab48c72b35475e83116e462477b72e
Vanessa Roque <viroque19@gmail.com>
2023-10-22 11:50:55 +0000

    add solution 😎

---
+++ hacker-rank/migratoryBirds.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    sightings = []
+    for i in range(1, 6):
+        sightings.append(arr.count(i))
+    return sightings.index(max(sightings))+1
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
