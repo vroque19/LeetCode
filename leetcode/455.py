02b8fbc5d1af1fe99f53ad9432ad8a0351c6ffc7
Vanessa Roque <viroque19@gmail.com>
2023-10-29 14:34:33 +0000

    add assign cookies

---
+++ leetcode/455.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = 0
+        j = 0
+        content = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                content += 1
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return content
+            
