dd563ff1d4758758ecbb4246ce2d1ed560c26ec8
Vanessa Isabel Roque <vroque19@csu.fullerton.edu>
2023-11-16 10:38:50 +0900

    add max element after sorting and rearranging

---
+++ leetcode/1846.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if abs(arr[i] - arr[i-1]) <= 1:
+                continue
+            arr[i] = arr[i-1] + 1
+        return arr[len(arr)-1]
