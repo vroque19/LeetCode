394e8259c6b9512caf1530c671024caa0fcf3588
Vanessa Roque <viroque19@gmail.com>
2023-12-14 06:43:22 +0000

    add getSecondLargest

---
+++ hacker-rank/loops.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let res = nums[0];
+    nums = nums.sort((a, b) => a - b);
+    let mySet = new Set(nums);
+    nums = Array.from(mySet);
+    // console.log(mySet)
+    if(nums.length > 1) {        
+        res = nums[nums.length - 2];
+    }
+    
+    return res;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
