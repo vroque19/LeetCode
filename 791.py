bb4a3049395dd80ce0fa4057ff76705f00d6f272
Vanessa Isabel Roque <vroque19@csu.fullerton.edu>
2024-03-11 15:22:43 -0700

    add customSortString

---
+++ 791.py
@@ -0,0 +1,20 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        cntr = Counter(s)
+        new = ""
+        for i in range(len(order)):
+            i = i
+            c = order[i]
+            if c in s:
+                new+=c*cntr[c]
+                cntr[c] -= cntr[c]
+            else:
+                continue
+        for k, v in cntr.items():
+            while v > 0:
+                new += k
+                v -= 1
+            continue
+
+        return new
+        
