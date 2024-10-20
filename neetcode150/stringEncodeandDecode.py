347b93d126cd64fe796dbec94762d28abb98444c
Vanessa Roque <viroque19@gmail.com>
2024-10-20 06:57:43 +0000

    stringEncodeandDecode

--- neetcode150/stringEncodeandDecode.py
+++ neetcode150/stringEncodeandDecode.py
@@ -3,4 +3,22 @@ class Solution:
         return str(strs)
     def decode(self, s: str) -> List[str]:
         return eval(s)
+"""
+class Solution:
+    lengths = []
+    def encode(self, strs: List[str]) -> str:
+        for s in strs:
+            Solution.lengths.append(len(s))
+        return "".join(strs)
+    def decode(self, s: str) -> List[str]:
+        res = []
+        p = 0
+        arr = Solution.lengths
+        print(arr, s)
+        for i in range(len(arr)):
+            res.append(s[p:p+arr[i]] if arr[i] != 0 else "")
+            p += arr[i]
+        Solution.lengths = []
+        return res
         
+"""
