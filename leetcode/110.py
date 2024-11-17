ecf9aefbf6561c043a22846df395a14ee613c058
Vanessa Roque <viroque19@gmail.com>
2024-11-17 07:12:51 +0000

    isBalanced new sol

--- leetcode/110.py
+++ leetcode/110.py
@@ -4,20 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.checkBalance(root)
-        return self.isBalanced
-
-    def checkBalance(self, root):
-        if root is None:
-            return 0
-        leftHeight = self.checkBalance(root.left)
-        rightHeight = self.checkBalance(root.right)
-
-        if abs(leftHeight - rightHeight) > 1:
-            self.isBalanced = False
-
-        return max(leftHeight, rightHeight) + 1 
-        
-        
+        def isBalancedHelper(root):
+            if root is None:
+                return [True, 0]
+            bal = False
+            left = isBalancedHelper(root.left)
+            right = isBalancedHelper(root.right)
+            h = 1 + max(left[1], right[1])
+            if abs(left[1]-right[1])<=1 and left[0] and right[0]:
+                bal = True
+            return [bal, h]
+        return isBalancedHelper(root)[0]
