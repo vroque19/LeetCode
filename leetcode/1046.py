92d9e92dd880b940e03e2723bff9a89fd0c666fa
Vanessa Roque <viroque19@gmail.com>
2024-11-23 20:47:12 +0000

    Last Stone Weight using maxheap

--- leetcode/1046.py
+++ leetcode/1046.py
@@ -2,10 +2,13 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         if len(stones) == 0:
             return 0
-        if len(stones) == 1:
-            return stones[0]
-        stones = sorted(stones)
-        new_stone = stones[len(stones)-1] - stones[len(stones)-2]
-        stones = stones[:len(stones)-2]
-        stones.append(new_stone)
-        return self.lastStoneWeight(stones)
+        
+        max_heap = [-stone for stone in stones]
+        heapq.heapify(max_heap)
+
+        while len(max_heap) > 1:
+            x = -heapq.heappop(max_heap)
+            y = -heapq.heappop(max_heap)
+            if x != 0:
+                heapq.heappush(max_heap, -abs(y-x))
+        return -max_heap[0] if max_heap else 0
