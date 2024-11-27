f5a05f4f1f76f5bd170d659b00fb5bdbfc7374b2
Vanessa Roque <viroque19@gmail.com>
2024-11-27 02:22:40 +0000

    clone graph

---
+++ leetcode/133.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return None
+
+        visited = {}
+        def cloneNode(node):
+            if node in visited:
+                return visited[node]
+            clone = Node(node.val)
+            visited[node] = clone
+
+            for neighbor in node.neighbors:
+                clone.neighbors.append(cloneNode(neighbor))
+            return clone
+
+        return cloneNode(node)
+        
+
+
+                
+        
