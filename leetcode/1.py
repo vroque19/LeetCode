3444f77c7794e9632fff72f49aebb32e7e9b0350
Vanessa Roque <viroque19@gmail.com>
2025-02-05 19:54:09 +0000

    two sum

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,12 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h_map = {}
-        for i in range(len(nums)):
-            diff = target-nums[i]
-            if diff in h_map:
-                return [h_map[diff], i]
-            h_map[nums[i]] = i
-        return [] # no solution
+def two_sum(nums: list[int], target: int) -> tuple:
+    hash_map = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hash_map:
+            return (hash_map[complement], i)
+        hash_map[nums[i]] = i
+    return
 
 
-        
+"""
+target = 3
+"""
+nums = [1, 3, 6, 5, 2]
+
+res = two_sum(nums, 3)
+
+print(res)
