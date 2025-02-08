ca3911ad6c9c5bdaa1244ae42f749346386fac12
Vanessa Roque <viroque19@gmail.com>
2025-02-08 19:25:31 +0000

    add length of longest substring without duplicates

--- leetcode/3.py
+++ leetcode/3.py
@@ -1,14 +1,31 @@
 from collections import defaultdict
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        p1 = 0
-        hmap = defaultdict(lambda: 0)
-        longest = 0
-        for i in range(len(s)):
-            hmap[s[i]] += 1
-            while hmap[s[i]] > 1:
-                hmap[s[p1]] -= 1
-                p1 += 1
-            longest = max(longest, i - p1 + 1)
-        return longest
 
+
+def longest_substring_without_repeats(s: str):
+    if len(s) == 0 or len(s) == 1:
+        return len(s)
+    l, r = 0, 1
+    freq_map = defaultdict(lambda: -1)
+    print(freq_map)
+    # return
+    freq_map[s[l]] = l
+    largest_length = 1
+    # ## aabcacd
+    # ##      lr
+    while r < len(s):
+        if freq_map[s[r]] != -1:
+            for i in range(l, freq_map[s[r]]):
+                freq_map[s[i]] = -1
+            print(l, freq_map[s[r]])
+            l = freq_map[s[r]] + 1
+            freq_map[s[l]] = l
+        freq_map[s[r]] = r
+        r += 1
+        largest_length = max(largest_length, r - l)  # ll = 3
+
+    print(r, l, largest_length)
+    return largest_length
+
+
+res = longest_substring_without_repeats("aabcabd")
+print(res)
