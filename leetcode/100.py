a8058dc6ceb421b28073123c9dfccd4065e3d362
Vanessa Roque <viroque19@gmail.com>
2025-11-03 08:33:24 -0800

    add middle of linked list

--- leetcode/100.py
+++ leetcode/100.py
@@ -4,14 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None:
-            return True
-        if p is None or q is None:
-            return False
-        if p.val != q.val:
-            return False
-        left = self.isSameTree(p.left, q.left)
-        right = self.isSameTree(p.right, q.right)
-        return left and right
+        # bfs each tree at the same time
+        # if at any point the current nodes of each tree dont match return false
+        if not p or not q:
+            return p == q
+        qp = deque()
+        qq = deque()
+        qp.append(p)
+        qq.append(q)
+        
+        while qq and qp:
+            if qp[0].left and qq[0].left:
+                qp.append(qp[0].left)
+                qq.append(qq[0].left)
+            elif not qp[0].left and not qq[0].left:
+                pass
+            else:
+                return False
+            if qp[0].right and qq[0].right:
+                qp.append(qp[0].right)
+                qq.append(qq[0].right)
+            elif not qp[0].right and not qq[0].right:
+                pass
+            else:
+                print(qq[0], qp[0].right)
+                return False
+            if qp.popleft().val != qq.popleft().val:
+                return False
+        return True
--- leetcode/876.py
+++ leetcode/876.py
@@ -5,18 +5,14 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head.next is None:
-            return head
-        slow = head
-        fast = head.next
-        found = False
-        while not found:
-            if fast.next is None or fast.next.next is None:
-                found = True
-            else:
-                slow = slow.next
-                fast = fast.next.next
-        return slow.next
+        trailing, leading = head, head.next
+        if leading is None:
+            return trailing
+        while leading.next:
+            trailing = trailing.next
+            leading = leading.next.next
+            if leading is None:
+                return trailing
+        return trailing.next
+
         
-
-
